d33d1c5e3bf9ddd2327b1ccaf3cb41ea22070319
DuanFaxin <2736913400@qq.com>
2024-03-30 22:30:12 +0800

    前端显示测试

--- HttpServer/httpServer.cpp
+++ HttpServer/httpServer.cpp
@@ -1,11 +1,23 @@
 #include <pistache/endpoint.h>
-
+#include <string>
+#include <fstream>
 using namespace Pistache;
 
 struct HelloHandler : public Http::Handler{
     HTTP_PROTOTYPE(HelloHandler);
     void onRequest(const Http::Request&, Http::ResponseWriter writer) override {
-        writer.send(Http::Code::Ok,"Hello,world!");
+        std::string index = "";
+        std::ifstream file("../index.html");
+        if(!file.is_open())
+        {
+           std::cerr << "无法打开文件" << std::endl;
+           return ;
+        }
+         std::string line;
+        while (getline(file, line)) { // 逐行读取
+            index += line;
+        }
+        writer.send(Http::Code::Ok,index);
     }
 };
 
@@ -13,4 +25,5 @@ struct HelloHandler : public Http::Handler{
 int main() {
   Http::listenAndServe<HelloHandler>(Pistache::Address("*:9080"));
 }
+
 
